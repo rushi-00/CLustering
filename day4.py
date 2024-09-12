e28a756aba9aa42917d41eeaeda9f3190284ac56
Rushikesh Chobhe <rushi141414@gmail.com>
2024-09-12 22:53:03 +0530

    Add files via upload

---
+++ day4.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Aug 13 09:42:41 2024
+
+@author: SATYAM
+"""
+
+from sklearn.cluster import KMeans
+import pandas as pd
+import numpy as np
+from matplotlib import pyplot as plt
+
+ 
